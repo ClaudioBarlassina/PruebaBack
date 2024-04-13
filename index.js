da2e046becad5a8388e61f04fcd96cad1588d3ae
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-04-13 01:28:09 -0300

    render prueba

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// const server = require("./src/server.js")
-const sequelize = require("./sequelize.js")
+const server = require("./src/server.js")
+
 const {conn} = require("./src/db.js")
-const PORT = 3500;
+const PORT = 3001;
 
 
 //-----------------------------------------------------------------------------------------------------
 
-conn.sync().then(() => {
+// conn.sync().then(() => {
 
-    sequelize.sync()
-  .then(() => {
-    console.log('Database synced successfully');
-    // Tu lógica de aplicación aquí
+ 
+  server.listen(PORT, () => {
+      console.log(`server listening on port ${PORT}`)   //` ` COMILLAS ALT+96
   })
+   
+  // })
     
-    // server.listen(PORT, () => {
-    //     console.log(`server listening on port ${PORT}`)   //` ` COMILLAS ALT+96
-    // })
-})
