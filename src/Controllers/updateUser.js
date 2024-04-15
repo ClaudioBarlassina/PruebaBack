d6038129ae099e0d3aec2d4988dc12eac0cb40ad
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-04-14 23:24:49 -0300

    render prueba

--- src/Controllers/updateUser.js
+++ src/Controllers/updateUser.js
@@ -3,9 +3,9 @@ const { USUARIOS } = require("../db")
 const updateUsers = async (userData, usersID) => {
     
     const users = await USUARIOS.findByPk(usersID);
-    
-   const New = await users.update(userData)
-    return New;
+    await users.update(userData)
+    return users
+  
 }
 
 module.exports = updateUsers;
